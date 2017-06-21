5d1ee4e3a25f8ced38d066f666b4883225ea5587
mkucaj <mkucaj86@gmail.com>
2017-06-21 14:26:25 +0200

    styling shoping cart items

--- api/products.js
+++ api/products.js
@@ -3,7 +3,7 @@ const products = [
     { "id": "1","qty": "1", "title": "Ipad 5", "description": "lorem ipsum some test dimpsum", "img": "iphone5.jpg", "price": "300", "inventory": "3" , "totalPrice": "300", "outOfStock": false},
     { "id": "2","qty": "1", "title": "Iphone 6", "description": "lorem ipsum some test dimsum", "img": "iphone6.png", "price": "450", "inventory": "6" , "totalPrice": "450", "outOfStock": false},
     { "id": "3","qty": "1", "title": "htcOne", "description": "lorem ipsum some test dimsum", "img": "htc.png", "price": "420", "inventory": "5" , "totalPrice": "420", "outOfStock": false},
-    { "id": "4", "qty": "1","title": "huawe-P10", "description": "lorem ipsum some test dimsum", "img": "huawei-p10.jpg", "price": "240", "inventory": "10", "totalPrice": "240" , "outOfStock": false}
+    { "id": "4", "qty": "1","title": "huawei-P10", "description": "lorem ipsum some test dimsum", "img": "huawei-p10.jpg", "price": "240", "inventory": "10", "totalPrice": "240" , "outOfStock": false}
 ];
 
 
--- components/shop/shoppingCart.vue
+++ components/shop/shoppingCart.vue
@@ -2,8 +2,16 @@
   <section>
     <h2>{{title}}</h2>
     <div v-for="item in items">
-    	<span>{{item.title}}</span>
-    	<span>  {{item.qty}}</span>
+    	<div class="cart-item">
+			<div class="cart-item-details cart-item-img">
+				<img v-bind:src="`img/${item.img}`" class="img-responsive" alt="Sample App Products" />
+			</div>
+			<div class="cart-item-details-box">
+    			<div class="cart-item-details cart-item-title">{{item.title}}</div>
+    			<div class="cart-item-details">Quantity: {{item.qty}}</div>
+    			<div class="cart-item-details">Price: {{item.price}}</div>
+    		</div>
+    	</div>
     </div>
   </section>
 </template>
@@ -23,5 +31,27 @@ export default {
 </script>
 
 <style scoped>
-
+.cart-item{
+	display: inline-block;
+	width: 100%;
+}
+.cart-item-details{
+	display: inline-block;
+	text-align: left;
+	width: 100%;
+}
+.cart-item-title{
+	font-weight: 700;
+}
+.cart-item-img{
+	display: block;
+	float: left;
+	width: 40%;
+}
+.cart-item-details-box{
+	display: block;
+	float: left;
+	padding: 0 0 0 5px;
+	width: 60%;
+}
 </style>
